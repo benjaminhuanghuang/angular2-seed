356ac112dacd2991aff64ac39dfdbd328f376289
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-03-14 22:34:17 -0700

    use component in component

---
+++ src/courses.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { Directive} from '@angular/core';
+@Component({
+  selector: 'courses',
+  template: '<h2>courses</h2>',
+})
+export class CoursesComponent  
+{ }
