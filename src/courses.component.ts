220060a83e432e031aed33403ff69b9541f96ab9
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-03-14 22:48:46 -0700

    use component property and for loop

--- src/courses.component.ts
+++ src/courses.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
-import { Directive} from '@angular/core';
+import { Directive } from '@angular/core';
 @Component({
-  selector: 'courses',
-  template: '<h2>courses</h2>',
+    selector: 'courses',
+    template: `
+                <h2>courses</h2>
+                {{title}}
+                <ul>
+                    <li *ngFor="let item of courses; let i = index">{{i}} {{item}}</li>
+                </ul>
+              `,
 })
-export class CoursesComponent  
-{ }
+export class CoursesComponent {
+    title: string = "The title of courses page";
+    courses = ["Course1", "Course2", "Course3" ];
+}
